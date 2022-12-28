cc85285f43582ccd2ac9ed53503721e8494f6cb1
Viktor Bredenko <viktor.bredenko@gmail.com>
2022-12-28 07:29:17 +0200

    1

--- task/ulbi-task/105-callback-ulbi.js
+++ task/ulbi-task/105-callback-ulbi.js
@@ -1,16 +1,30 @@
 //https://www.youtube.com/watch?v=G7pAP1TvZSw
+//  { '4': [ 4.2 ], '6': [ 6.1, 6.3 ] }
 
 const groupBy = (arr, callbackFunction) => {
-    const result ={}
+    const result = {}
+
     arr.forEach(item => {
         let resCallback = callbackFunction(item)
-        console.log(resCallback)
-        
+        console.log(result[resCallback])
+
         result[resCallback]
             ? result[resCallback].push(item)
-            : result[resCallback] = [item]        
+            : result[resCallback] = [item]
     });
     //console.log(result)
 }
 
-groupBy( [6.1, 4.2, 6.3], Math.floor)
+groupBy([6.1, 4.2, 4.9, 6.3], Math.floor)
+
+
+// let o = {}
+
+// key = null;
+
+// o[key] ? console.log(true) : console.log(true)
+
+
+//console.log(o)
+
+
